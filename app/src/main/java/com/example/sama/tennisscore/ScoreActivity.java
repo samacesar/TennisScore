11cef3a92d27f05b4bb5676a53a1d3e0c7d75322
sama <00013814@uca.edu.sv>
2018-03-24 16:24:06 -0600

    first commint

---
+++ app/src/main/java/com/example/sama/tennisscore/ScoreActivity.java
@@ -0,0 +1,20 @@
+package com.example.sama.tennisscore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+    }
+}
+/*
+3 FACTORES
+GAME
+SET
+MATCH
+
+ */
